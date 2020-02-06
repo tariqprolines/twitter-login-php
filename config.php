4f7bf09700193497aeb8721ebaf5b8382059fd91
tariqprolines <tariq.prolines.sa@gmail.com>
2020-02-06 13:43:08 +0300

    first commit

---
+++ config.php
@@ -0,0 +1,5 @@
+	<?php
+	define('CONSUMER_KEY', 'HZ9kosou2RrfF7ZzZXlGCSVLS');
+	define('CONSUMER_SECRET', '5O08LJHBUe17UdLE4eZX4fi0PB9tIDT03JsSfpjWyRaTSBMTka');
+	define('OAUTH_CALLBACK', 'http://localhost/twitterlogin/process.php');
+	?>
